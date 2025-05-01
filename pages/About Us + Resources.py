f445c6db881458bf40791808e50be7a17fa8d476
kbw7 <kb120@wellesley.edu>
2025-05-01 07:37:41 -0400

    fixed bullets

--- pages/About Us + Resources.py
+++ pages/About Us + Resources.py
@@ -4,6 +4,6 @@ st.write("WellesleyCrave is an app made by CS248 students: Kaurvaki, Aileen, and
 
 st.subheader("Contact Information")
 st.write("If you have any questions or concerns, please email any of us!")
-st.markdown("""* Kaurvaki Bajpai (kb120@wellesley.edu) 
-            * Aileen Du (ad128@wellesley.edu) 
-            * Rebecca Hsia (rh109@wellesley.edu)""")
+st.markdown("* Kaurvaki Bajpai (kb120@wellesley.edu)")
+st.markdown("* Aileen Du (ad128@wellesley.edu)")
+st.markdown("* Rebecca Hsia (rh109@wellesley.edu)")
