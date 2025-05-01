b0d42e725dc35b2179ff18d3e0f7d316526bbeed
kbw7 <kb120@wellesley.edu>
2025-05-01 07:34:50 -0400

    more info on resources page

--- pages/About Us + Resources.py
+++ pages/About Us + Resources.py
@@ -1,2 +1,7 @@
 import streamlit as st
-st.write("WellesleyCrave is an app made my CS248 students: Kaurvaki, Aileen, and Rebecca. Our app's purpose is to make the food logging experience for Wellesley students a positive one! All of the information regarding food is sourced from Wellesley Fresh AVI.")
+st.subheader("Mission Statement")
+st.write("WellesleyCrave is an app made by CS248 students: Kaurvaki, Aileen, and Rebecca. Our app's purpose is to make the food logging experience for Wellesley students a positive one! All of the information regarding food is sourced from Wellesley Fresh AVI.")
+
+st.subheader("Contact Information")
+st.write("If you have any questions or concerns, please email any of us!")
+st.markdown("* Kaurvaki Bajpai (kb120@wellesley.edu) * Aileen Du (ad128@wellesley.edu) * Rebecca Hsia (rh109@wellesley.edu)")
