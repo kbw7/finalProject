cc9bb56d1d45c6eb77542c4170ade938e2c87775
kbw7 <kb120@wellesley.edu>
2025-05-06 13:18:39 -0400

    somehow pd forgotten?

--- home.py
+++ home.py
@@ -230,7 +230,7 @@ def homePage(): # only show once user has walkthrough!
     # Source for strftime - https://www.geeksforgeeks.org/python-strftime-function/
 
     today = d.strftime("%Y") + "-" + d.strftime("%m") + "-" + d.strftime("%d") + "T00:00:00"
-    st.write(df)
+    st.write(pd.DataFrame(df))
     df = df[df["date"] == today]  # only today's meals
 
     # Aileen's Code
