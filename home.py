0512b9a78b782c1c1e05a6031a274614a77fe429
kbw7 <kb120@wellesley.edu>
2025-05-06 14:07:20 -0400

    fixing date error

--- home.py
+++ home.py
@@ -223,7 +223,9 @@ def homePage(): # only show once user has walkthrough!
 
     d = datetime.date.today()
 
-    df = get_menu(d, location_id, meal_id) # d is date
+    formattedDate = d.strftime("%m-%d-%Y")
+
+    df = get_menu(formattedDate, location_id, meal_id) # d is date
 
     # We only want today's menu... not the whole week
     # format of date data in df: 2025-04-14T00:00:00
@@ -240,7 +242,7 @@ def homePage(): # only show once user has walkthrough!
 
     # cleaning up df - Kaurvaki - lot of code from own Assignment 5 of CS248
     df = df.drop_duplicates(subset=["id"], keep="first")
-    df = df.drop(columns=["date", "image", "id", "categoryName", "stationOrder", "price"], errors="ignore")
+    df = df.drop(columns=["image", "id", "categoryName", "stationOrder", "price"], errors="ignore")
 
     df["allergens"] = df["allergens"].apply(transform) # for the allergens column, it goes through each row and turn them into a string
     df["preferences"] = df["preferences"].apply(transform)
